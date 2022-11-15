28176b16a7fae077a7770953f1803ac665f841f5
Fawas Kareem <phawazzzy@gmail.com>
2022-11-16 00:28:26 +0100

    serverless project done

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '81kp1wsh04'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-sys6ldag10n44b8i.us.auth0.com',
+  clientId: '5y1pSg7BoOVrU8bxB7Obcg4f17y9sgOA',
+  callbackUrl: 'http://localhost:3000/callback'
+}
